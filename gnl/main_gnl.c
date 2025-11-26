0a186a58debdabed86a17367eacf0a7044badba2
rek1-420 <141921344+rek1-420@users.noreply.github.com>
2025-11-26 16:05:20 +0100

    Update main_gnl.c

--- gnl/main_gnl.c
+++ gnl/main_gnl.c
@@ -1,33 +1,34 @@
-#include "get_next_line.h"
 #include <fcntl.h>
 #include <stdio.h>
 
 int	main(void)
 {
 	int fd;
-    int nbr_line;
+	int nbr_line;
 	char *s;
 
-    nbr_line = 5;
+	nbr_line = 5;
 
 	// fd = 0;
 	fd = open("test.txt", O_RDONLY);
 
-///////////////////////// LIS nbr_line LIGNES /////////////////////////
-    while (nbr_line > 0)
-    {
-        s = get_next_line(fd);
-        printf("nl = %s", s);
-        free(s);
-        nbr_line--;
-    }
+	///////////////////////// LIS nbr_line LIGNES /////////////////////////
+	while (nbr_line > 0)
+	{
+		s = get_next_line(fd);
+		printf("nl = %s", s);
+		printf("\n");
+		free(s);
+		nbr_line--;
+	}
 
-///////////////////////// LIS TOUT LE FICHIER /////////////////////////
-    // while ((s = get_next_line(fd)))
-    // {
-    //     printf("nl = %s", s);
-    //     free(s);
-    // }
+	///////////////////////// LIS TOUT LE FICHIER /////////////////////////
+	// while ((s = get_next_line(fd)))
+	// {
+	// 	printf("nl = %s", s);
+	// 	printf("\n");
+	// 	free(s);
+	// }
 
 	close(fd);
 	return (0);
